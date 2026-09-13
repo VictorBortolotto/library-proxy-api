68c6eb9e1f029e0c29228384b836b375caff2aa3
VictorBortolotto <victorcamposbortolotto@gmail.com>
2026-09-13 19:34:05 -0300

    Remove print

--- br/com/app/src/main/auth/JwtAuth.py
+++ br/com/app/src/main/auth/JwtAuth.py
@@ -45,8 +45,6 @@ class JwtAuth:
   def get_token(self, request):
     token = request.headers.get('Authorization')
 
-    print("Authorization recebido:", token)
-    
     if token and token.startswith("Bearer "):
       token = token[7:]
 
